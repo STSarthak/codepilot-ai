bd7ac78aebdee83167b17ad55ec9c5ccf9df4360
Sarthak Taneja <sarthaktaneja28@gmail.com>
2026-04-23 23:34:40 +0530

    Initial commit: CodePilot AI project

---
+++ src/main/java/com/sarthak/projects/codepilot_ai/CodepilotAiApplication.java
@@ -0,0 +1,13 @@
+package com.sarthak.projects.codepilot_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodepilotAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodepilotAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarthak/projects/codepilot_ai/CodepilotAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarthak.projects.codepilot_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodepilotAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
